4ff2762ff14950d1c7ce7f0dfff529e942726111
Privilege Mendes <privilegemendes@gmail.com>
2023-02-27 15:31:59 +0100

    added caesars-cipher

---
+++ JavaScript/caesars-cipher/index.js
@@ -0,0 +1,49 @@
+function caesar(input, rot) {
+  if (typeof input !== 'string') {
+    throw new TypeError("It's not a String!");
+  }
+  if (input.length < 1) {
+    throw new Error('The string is empty!');
+  }
+
+  const inputArray = [
+    ...input
+  ];
+  const result = [];
+
+  inputArray.forEach(char => {
+    const isUpperCase = char === char.toUpperCase();
+    const isChar = !!char.match(/[a-z]|[A-Z]/g);
+
+    if (isChar) {
+      const newChar = findNewChar(char, rot);
+      result.push(isUpperCase ? newChar.toUpperCase() : newChar);
+    } else {
+      result.push(char);
+    }
+  });
+  return result.join('');
+}
+
+function findNewChar(char, rot) {
+  const theRot = +rot;
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const idx = alphabet.indexOf(char.toLowerCase());
+  let newIdx;
+
+  if (idx + theRot > alphabet.length - 1) {
+    newIdx = theRot - (alphabet.length - idx);
+  } else {
+    newIdx = idx + theRot;
+  }
+  return alphabet[newIdx];
+}
+
+function rot13(str) {
+
+  return caesar(str, 13);
+}
+
+rot13("SERR PBQR PNZC");
+
+console.log(rot13("SERR CVMMN!"));
